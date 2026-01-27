48d5fe09b7852b40464ffea80110d4e73d9685fa
zcag <cagdassalur@gmail.com>
2026-01-28 01:15:19 +0300

    Noti on clipcopy

--- examples/plugins/actions/copy/run.sh
+++ examples/plugins/actions/copy/run.sh
@@ -9,6 +9,14 @@ run() {
   elif command -v pbcopy &>/dev/null; then
     printf '%s' "$value" | pbcopy
   fi
+
+  # Show notification
+  # Truncate for display
+  display="${value:0:50}"
+  [[ ${#value} -gt 50 ]] && display="${display}..."
+  if command -v notify-send &>/dev/null; then
+    notify-send -t 2000 "Copied" "$display"
+  fi
 }
 
 CMD=$1; shift
