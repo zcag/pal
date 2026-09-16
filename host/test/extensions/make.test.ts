5b8f36f3e249f3c813d6a9cc61925e37c8f76617
zcag <cagdassalur@gmail.com>
2026-09-17 00:36:39 +0300

    make test: find the script whichever terminal the box has (Terminal.app on CI)

--- host/test/extensions/make.test.ts
+++ host/test/extensions/make.test.ts
@@ -97,7 +97,8 @@ describe("make", () => {
     await list();
     expect(await pick(id("test", pal))).toEqual({ hud: "make test" });
     const argv = JSON.parse(readFileSync(join(root, "terminal"), "utf8").trim().split("\n").at(-1)!) as string[];
-    const script = argv.at(-1)!;
+    // kitty takes the script as its last argument; Terminal.app (the CI runner) wraps it in an AppleScript `do script` line.
+    const script = argv.find((a) => a.includes("make test"))!.replace(/^tell application "Terminal" to do script "/, "").replace(/\\"/g, '"');
     expect(script).toStartWith(`cd ${pal} && exec sh -c 'make test; s=$?;`);
     expect(script).toContain("read -r _");
     expect(await pick(id("a", pal), "run")).toEqual({ hud: "make a" });
